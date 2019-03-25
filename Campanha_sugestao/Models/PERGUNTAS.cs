3e3ff9796b38389b2662b8d5aaa0e563fdf541ab
Rafael Silva <Rafaell_21@hotmail.com>
2019-03-25 10:05:33 -0300

    primeiro commit

---
+++ Campanha_sugestao/Models/PERGUNTAS.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations
+
+namespace Campanha_sugestao.Models
+{
+    public class PERGUNTAS
+    {
+        
+        public int Id_perguntas { get; set; }
+        
+        public int Id_Campanha { get; set; }
+       
+        public string Pergunta1 { get; set; }
+
+       
+        public string Pergunta2 { get; set; }
+
+       
+        public string Pergunta3 { get; set; }
+
+        
+        public string Pergunta4 { get; set; }
+
+       
+        public string Pergunta5 { get; set; }
+
+        
+        public string Pergunta6 { get; set; }
+
+        
+        public string Pergunta7 { get; set; }
+
+    }
+}
---
+++ Campanha_sugestao/Models/RESPONSAVEL.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations
+
+namespace Campanha_sugestao.Models
+{
+    public class RESPONSAVEL
+    {
+        [Required(ErrorMessage = "O id do responsável é obrigatorio", AllowEmptyStrings = false)]
+        [Display(Name = "Digite o código de identificação do funcionário")]
+        [StringLength 6, MinimumLength = 6)]
+        public int Id_responsavel { get; set; }
+        public int Id_funcionario { get; set; }
+        public int Id_empresa { get; set; }
+        
+    }
+}
